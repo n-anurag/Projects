45783229eb1e8d995c41f6149b87a997a25b88d5
anurag <anuragneupane58@gmail.com>
2025-01-13 19:09:11 +0545

    deleted

--- script.py
+++ script.py
@@ -12,23 +12,11 @@ def add_to_github(project_name):
     project_path = os.path.join(PARENT_FOLDER, project_name)
     os.chdir(project_path)  # Change directory to the project folder
 
-    # Check if the project is already a Git repository
-    if not os.path.exists(os.path.join(project_path, ".git")):
-        # Initialize Git and add remote repository
-        subprocess.run(["git", "init"])  # Initialize Git
-        subprocess.run(["git", "add", "."])  # Add all files
-        subprocess.run(["git", "commit", "-m", f"Initial commit for {project_name}"])  # Commit files
-        subprocess.run(["git", "branch", "-M", "main"])  # Rename the branch to 'main'
-        # Add the remote repository (use the single "Projects" repository)
-        subprocess.run(["git", "remote", "add", "origin", f"https://github.com/n-anurag/Projects.git"])
-        subprocess.run(["git", "push", "-u", "origin", "main"])  # Push to GitHub
-        print(f"Project {project_name} pushed to GitHub.")
-    else:
-        # If it's already a Git repository, update changes
-        subprocess.run(["git", "add", "."])  # Add all files
-        subprocess.run(["git", "commit", "-m", "Updated project"])  # Commit changes
-        subprocess.run(["git", "push"])  # Push updates to GitHub
-        print(f"Project {project_name} updated on GitHub.")
+     # If it's already a Git repository, update changes
+    subprocess.run(["git", "add", "."])  # Add all files
+    subprocess.run(["git", "commit", "-m", "Updated project"])  # Commit changes
+    subprocess.run(["git", "push"])  # Push updates to GitHub
+    print(f"Project {project_name} updated on GitHub.")
 
 def main():
     # Specify the subfolder you want to push
